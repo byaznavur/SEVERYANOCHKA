9ad9392c7eafdeeae06b64952131afd7a1591ca4
azizdev28 <az1z.bakht1yarvich@gmail.com>
2024-12-13 22:52:59 +0500

    pagination and search

--- js/products.js
+++ js/products.js
@@ -33,15 +33,44 @@ function getAllProducts(el) {
   </div>`;
 }
 let search = "";
+let activePage = 1;
 products.map((el) => {
   allProductsCards.innerHTML += getAllProducts(el);
 });
 function getProducts() {
   let results = products.filter((el) => el.name.toLowerCase().includes(search));
 
+  let pages = Math.ceil(results.length / 10);
+
+  if (pages > 1) {
+    pagination.innerHTML = `<li>
+    <button onClick =getPage("-") ${
+      activePage == 1 ? "disabled" : ""
+    }>Previus</button>
+    </li>`;
+
+    for (let i = 1; i <= pages; i++) {
+      pagination.innerHTML += `<li class = "${
+        i === activePage ? "active" : ""
+      }" >
+       <button  onClick = "getPage(${i})">${i}</button>
+       </li>`;
+    }
+    pagination.innerHTML += `<li>
+     <button onClick = getPage("+") ${
+       activePage == pages ? "disabled" : ""
+     }>Next</button>
+     </li>`;
+  } else {
+    pagination.innerHTML = "";
+  }
+  let startIndex = (activePage - 1) * 10;
+  let endIndex = activePage * 10;
+  let pageResults = results.slice(startIndex, endIndex);
+
   allProductsCards.innerHTML = "";
 
-  results.map((el) => {
+  pageResults.map((el) => {
     allProductsCards.innerHTML += getAllProducts(el);
   });
   countProducts.textContent = results.length;
@@ -49,6 +78,19 @@ function getProducts() {
 getProducts();
 
 searchProduct.addEventListener("keyup", function () {
+  activePage = 1;
   search = this.value.trim().toLowerCase();
   getProducts();
 });
+
+function getPage(page) {
+  if (page == "+") {
+    activePage++;
+  } else if (page == "-") {
+    activePage--;
+  } else {
+    activePage = page;
+  }
+  getProducts();
+  console.log(page);
+}
